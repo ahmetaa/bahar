a462a42484b48f37be12e4b3342f15fa207c95b3
ahmetaa <ahmetaa@2f79b658-d7fa-11dd-9042-6d746b85d38b>
2009-01-05 04:23:55 +0000

    cesitli duzeltmeler

--- trunk/src/bahar/bilgi/Klavyeler.java
+++ trunk/src/bahar/bilgi/Klavyeler.java
@@ -33,25 +33,27 @@ public class Klavyeler {
         String[] sagElDagilimi = {"", "7yhn8ujm&YHN*UJM", "9ik,(IK<", "0ol.)OL>", "-=p[]'/_+P{}:\"?"};
         return uret("Turkce Q", shift, normal, altGr, sagElDagilimi, solElDagilimi);
     }
-    
+
     // Turkce ozel
-    public static final char CHAR_CC = '\u00c7'; // Kuyruklu buyuk c (ch)
+
+
     public static final char CHAR_cc = '\u00e7'; // Kuyruklu kucuk c (ch)
-    public static final char CHAR_GG = '\u011e'; // Buyuk yumusak g
     public static final char CHAR_gg = '\u011f'; // Kucuk yumusak g
     public static final char CHAR_ii = '\u0131'; // Noktassiz kucuk i
-    public static final char CHAR_II = '\u0130'; // Noktali buyuk i
-    public static final char CHAR_OO = '\u00d6'; // Noktali buyuk o
     public static final char CHAR_oo = '\u00f6'; // Noktali kucuk o
-    public static final char CHAR_SS = '\u015e'; // Kuyruklu buyuk s (sh)
     public static final char CHAR_ss = '\u015f'; // Kuyruklu kucuk s (sh)
-    public static final char CHAR_UU = '\u00dc'; // Noktali buyuk u
     public static final char CHAR_uu = '\u00fc'; // Noktali kucuk u
 
+    public static final char CHAR_CC = '\u00c7'; // Kuyruklu buyuk c (ch)
+    public static final char CHAR_GG = '\u011e'; // Buyuk yumusak g
+    public static final char CHAR_II = '\u0130'; // Noktali buyuk i
+    public static final char CHAR_OO = '\u00d6'; // Noktali buyuk o
+    public static final char CHAR_SS = '\u015e'; // Kuyruklu buyuk s (sh)
+    public static final char CHAR_UU = '\u00dc'; // Noktali buyuk u
+
     public static Klavye turkceF() {
-        // TODO: HENUZ TANIMLANMADI.
-        String shift = "+!\"^$%&'()=?-QWERTYUIOP\u011e\u00dcASDFGHJKL\u015e\u0130>ZXCVBNM\u00d6\u00c7:;";
-        String normal = "\"1234567890*-qwertyu\u0131op\u011f\u00dcasdfghjkl\u015fi,<zxcvbnm\u00f6\u00e7. ";
+        String shift = "+!\"^$%&'()=?_QWERTYUIOP\u011e\u00dcASDFGHJKL\u015e\u0130>ZXCVBNM\u00d6\u00c7:;";
+        String normal = "*1234567890/-qwertyu\u0131op\u011f\u00dcasdfghjkl\u015fi<zxcvbnm\u00f6\u00e7.,";
         String altGr = "";
         String[] sagElDagilimi = {" ", "5%\u0131IaAcC6&oO\u00dc\u00fc\u00e7\u00c7", "4$\u011f\u011eeEvV", "3^gGi\u0130\u00f6\u00d6", "*+1!2\"fFuU<>jJ"};
         String[] solElDagilimi = {"", "7'dDtTzZ8(rRkKsS", "9)nNmMbB", "0=hHlL.:", "/?-_pPqQwWyY\u015e\u015fxX,;"};
